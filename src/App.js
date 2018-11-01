462e530423923da7294aa1762f58595e2308414e
Dmkurup <deepthikurupmk@gmail.com>
2018-11-01 12:38:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React , { Component } from 'react';
 import './App.css';
+import Clock from './Clock';
 
 class App extends Component {
-  render() {
-    return (
+constructor(props){
+super(props);
+this.state = {
+  deadLine: 'December 26,2018',
+  newDeadLine: ''
+}
+}
+
+changeDeadline(){
+ this.setState({deadLine: this.state.newDeadLine});
+}
+
+  render (){
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="AppTitle"> Count Down to {this.state.deadLine}</div>
+      <Clock deadLine={this.state.deadLine}/>
+      <div>
+      <input
+         placeholder="newDeadline"
+         onChange={event => this.setState({newDeadLine: event.target.value})}
+         />
+      <button onClick={()=>this.changeDeadline()}>
+      Submit
+      </button>
       </div>
-    );
+      </div>
+    )
   }
 }
 
---
+++ src/Clock.js
@@ -0,0 +1,45 @@
+import React , { Component} from 'react';
+
+class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+    console.log(this.props.deadLine);
+  }
+
+componentWillMount(){
+  this.getTimeUntil(this.props.deadLine);
+}
+
+componentDidMount(){
+  setInterval(() => this.getTimeUntil(this.props.deadLine), 1000 );
+}
+
+getTimeUntil(deadLine){
+  const time = Date.parse(deadLine) - Date.parse(new Date());
+  const seconds = Math.floor((time/1000)%60);
+  const minutes = Math.floor((time/1000/60)%60);
+  const hours = Math.floor(time/(1000*60*60)%24);
+  const days = Math.floor(time/(1000*60*60*24));
+  this.setState({days:days,hours:hours,minutes:minutes,seconds:seconds});
+}
+
+
+  render(){
+    return(
+      <div>
+      <div className="ClockTime">{this.state.days} days</div>
+      <div className="ClockTime">{this.state.hours} hours</div>
+      <div className="ClockTime">{this.state.minutes} minutes</div>
+      <div className="ClockTime">{this.state.seconds} seconds</div>
+      </div>
+    )
+  }
+}
+
+export default Clock;
